905ae83cd7ff72a25c333b3fba051d86400e8b6e
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-10-15 20:55:43 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Signin } from './src/pages/Signin';
+
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium }  from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold }  from '@expo-google-fonts/rajdhani';
 
 import { ThemeProvider } from 'styled-components';
-
 import { theme } from './src/global/styles/theme';
 
+import { Signin } from './src/pages/Signin';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ 
+    Inter_400Regular, 
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+  
   return (
     <ThemeProvider theme={theme}>
       <Signin />
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,5 +4,11 @@ export const theme = {
     heading: '#DDE3F0',
     primary: '#E51C44',
     line: '#991F36',
+  },
+  fonts: {
+    title700: 'Rajdhani_700Bold',
+    title500: 'Rajdhani_500Medium',
+    text400: 'Inter_400Regular',
+    text500: 'Inter_500Medium',
   }
 }
