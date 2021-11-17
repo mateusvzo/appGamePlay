751877383935f9eb0ec2dfce3617716de7cc1740
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-11-16 23:57:13 -0300

    Created Navigation Routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Home } from '../pages/Home';
+import { Signin } from '../pages/Signin';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen name="Signin" component={Signin}/>
+      <Screen name="Home" component={Home}/>
+    </Navigator>
+  )
+}
